fbef5a58345acef4acc6b29edd5c17288a3eda73
huyaqin666 <1184173192@qq.com>
2019-06-26 16:48:25 +0800

    第san次

--- app/src/main/java/com/example/myapp/fragment/Fragment_Shoping.java
+++ app/src/main/java/com/example/myapp/fragment/Fragment_Shoping.java
@@ -1,16 +1,21 @@
 package com.example.myapp.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.example.myapp.R;
+import com.example.myapp.adapters.Adapter_Shoping;
 import com.example.myapp.base.BaseFragment;
 import com.example.myapp.interfaces.IPersenter;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,12 +33,14 @@ public class Fragment_Shoping extends BaseFragment {
     @BindView(R.id.re)
     RecyclerView re;
     @BindView(R.id.rb_btn)
-    RadioButton rbBtn;
+    CheckBox rbBtn;
     @BindView(R.id.rb_btn_1)
     TextView rbBtn1;
     @BindView(R.id.bianji)
     TextView bianji;
     Unbinder unbinder;
+    private ArrayList<String> list;
+    private Adapter_Shoping adapter_shoping;
 
     @Override
     protected int getLayoutId() {
@@ -47,7 +54,14 @@ public class Fragment_Shoping extends BaseFragment {
 
     @Override
     protected void initData() {
-
+        list = new ArrayList<>();
+        for (int i = 0; i <20 ; i++) {
+            list.add("北欧简约山形花抱");
+        }
+        re.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter_shoping = new Adapter_Shoping(getActivity(), list);
+        re.setAdapter(adapter_shoping);
+        adapter_shoping.notifyDataSetChanged();
     }
 
     @Override
