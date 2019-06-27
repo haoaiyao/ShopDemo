6f72f72bbe7dc06082be07e9968f8c7c0d7bcb5b
zhangYuTingCoCo <2696161362@qq.com>
2019-06-27 11:09:00 +0800

    Zyt4

--- app/src/main/java/com/example/myapp/model/apis/MyApi.java
+++ app/src/main/java/com/example/myapp/model/apis/MyApi.java
@@ -1,11 +1,8 @@
 package com.example.myapp.model.apis;
 
 import com.example.myapp.model.bean.IndexBean;
-<<<<<<< HEAD
 import com.example.myapp.model.bean.home.HomeBean;
-=======
 import com.example.myapp.model.bean.JujiaBeam;
->>>>>>> 4927d6cd37b638a5d6d96d00c2fccaa49c638490
 
 import io.reactivex.Flowable;
 import retrofit2.http.GET;
@@ -16,14 +13,10 @@ public interface MyApi {
     @GET("index/index")
     Flowable<IndexBean> getIndexData();
 
-<<<<<<< HEAD
     //首页
     @GET("index/index")
     Flowable<HomeBean> getHomeBean();
 
-=======
     @GET("catalog/index")
     Flowable<JujiaBeam> getJuJia();
->>>>>>> 4927d6cd37b638a5d6d96d00c2fccaa49c638490
-
 }
